081df3bd19a69b8507b27a708dbf9ae661ce1eed
leonm <leecoop@gmail.com>
2020-02-21 15:28:42 +0200

    add netflix hystrix for feign CircuitBreaker

--- user-service/src/main/java/com/microservices/tutorial/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/microservices/tutorial/userservice/UserServiceApplication.java
@@ -2,6 +2,7 @@ package com.microservices.tutorial.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -12,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableCircuitBreaker
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
--- user-service/src/main/java/com/microservices/tutorial/userservice/client/AccountManagementClient.java
+++ user-service/src/main/java/com/microservices/tutorial/userservice/client/AccountManagementClient.java
@@ -1,16 +1,48 @@
 package com.microservices.tutorial.userservice.client;
 
 import com.microservices.tutorial.accountmanagementservice.model.AccountDetails;
+import feign.hystrix.FallbackFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
 
 /**
  * Created by leonmat on 20/2/2020.
  */
-@FeignClient(name = "account-management-service")
+@FeignClient(name = "account-management-service", fallbackFactory = AccountManagementFallbackFactory.class)
 public interface AccountManagementClient {
 
     @GetMapping("/account/{userId}")
     AccountDetails getUserAccount(@PathVariable("userId") String userId);
+}
+
+
+
+@Component
+class AccountManagementFallbackFactory implements FallbackFactory<AccountManagementClient>{
+
+    @Override
+    public AccountManagementClient create(Throwable throwable) {
+        return new AccountManagementFallback(throwable);
+    }
+}
+
+class AccountManagementFallback implements AccountManagementClient{
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+    private Throwable throwable;
+
+    public AccountManagementFallback(Throwable throwable) {
+        this.throwable = throwable;
+    }
+
+    @Override
+    public AccountDetails getUserAccount(String userId) {
+        logger.error(throwable.getLocalizedMessage());
+        return null;
+    }
 }
